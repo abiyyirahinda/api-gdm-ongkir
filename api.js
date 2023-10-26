228fdb4d782fccf59533cc47b2989c3fd60aa98b
Abik <muhammadabiyyi73@gmail.com>
2023-10-26 17:43:02 +0700

    first commit

---
+++ api.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const app = express();
+const port = 3002;
+
+ // Ganti dengan API Key RajaOngkir Anda
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/city', async (req, res) => {
+  try {
+    const apiKey = process.env.API;
+    const response = await axios.get('https://api.rajaongkir.com/starter/city', {
+      headers: {
+        key: apiKey,
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send('Error fetching data from RajaOngkir API');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
